5844a4fa275b7462550af5c393de033dec6a8ee0
nellyequrbain <nellyequrbain@gmx.com>
2025-03-07 03:59:00 +0000

    Add 5.go

---
+++ src/5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 42
+	if num > 10 {
+		fmt.Println("The number is greater than 10")
+	} else {
+		fmt.Println("The number is less than or equal to 10")
+	}
+}
